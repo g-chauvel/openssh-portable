327a8578db816f3b31a53dc965d34cb8b93c99bf
Guillaume Chauvel <guillaume.chauvel@gmail.com>
2026-05-12 02:28:13 +0200

    F9: auto-quote arguments containing newline or vertical tab

--- contrib/win32/win32compat/misc.c
+++ contrib/win32/win32compat/misc.c
@@ -1962,7 +1962,8 @@ build_commandline_string(const char* cmd, char *const argv[], BOOLEAN prepend_mo
 			char * p1 = *t1++;
 			BOOL add_quotes = FALSE;
 			for (int i = 0; i < (int)strlen(p1); i++) {
-				if (p1[i] == ' ' || p1[i] == '\t') {
+				if (p1[i] == ' ' || p1[i] == '\t' ||
+				    p1[i] == '\n' || p1[i] == '\v') {
 					add_quotes = TRUE;
 					break;
 				}
